0b7b3e86179360554cbc68d146572e4296e2e526
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-11-17 12:09:56 +0100

    Add TeleportOnStart script

---
+++ ORST/Assets/Scripts/Core/Movement/Teleportation/TeleportOnStart.cs
@@ -0,0 +1,18 @@
+﻿using ORST.Foundation.Core;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace ORST.Core.Movement {
+    public class TeleportOnStart : BaseMonoBehaviour {
+        [SerializeField, Required] private AdvancedLocomotionTeleport m_AdvancedLocomotionTeleport;
+        [SerializeField, Required] private TeleportPointORST m_TeleportPoint;
+
+        private void Start() {
+            Assert.IsNotNull(m_AdvancedLocomotionTeleport, "AdvancedLocomotionTeleport is null");
+            Assert.IsNotNull(m_TeleportPoint, "TeleportPoint is null");
+
+            m_AdvancedLocomotionTeleport.Teleport(m_TeleportPoint);
+        }
+    }
+}
