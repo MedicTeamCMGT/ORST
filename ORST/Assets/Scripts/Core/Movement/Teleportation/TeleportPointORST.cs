1137b04228c4332ce69ce42c26f85c2f18019075
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-11-17 12:11:14 +0100

    Add reference to visuals in TeleportPoint

--- ORST/Assets/Scripts/Core/Movement/Teleportation/TeleportPointORST.cs
+++ ORST/Assets/Scripts/Core/Movement/Teleportation/TeleportPointORST.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 namespace ORST.Core.Movement {
     public class TeleportPointORST : BaseMonoBehaviour, ITeleportPoint {
         [SerializeField, Required] private Transform m_DestinationTransform;
+        [SerializeField, Required] private GameObject m_Visuals;
 
         public Transform DestinationTransform => m_DestinationTransform;
+        public GameObject Visuals => m_Visuals;
 
         private void OnEnable() {
             TeleportPointManager.RegisterPoint(this);
