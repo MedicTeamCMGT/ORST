1e0789d1a147305be61771f780fba0275267b6d3
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-11-23 12:14:45 +0100

    Add HandGrabStateProvider script
    This script provides an easy way to get a reference to a IHandGrabState
    for either the left or right hands, or for dominant and non-dominant
    hands. Without this you would need to keep track of when handedness
    changes and update your references to the new IHandGrabState.

---
+++ ORST/Assets/Scripts/Core/Interactions/Handedness/Interactors/HandGrabStateProvider.cs
@@ -0,0 +1,32 @@
+﻿using Oculus.Interaction;
+using Oculus.Interaction.HandGrab;
+using Oculus.Interaction.Input;
+using ORST.Foundation.Core;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEngine;
+
+namespace ORST.Core.Interactions {
+    public class HandGrabStateProvider : BaseMonoBehaviour, IHandGrabStateProvider {
+        [SerializeField] private PassthroughHand m_Hand = PassthroughHand.Dominant;
+        [OdinSerialize, Required] private IHandGrabState m_LeftHand;
+        [OdinSerialize, Required] private IHandGrabState m_RightHand;
+
+        public IHandGrabState Value => m_Hand switch {
+            PassthroughHand.Dominant when HandednessManager.Handedness is Handedness.Left => m_LeftHand,
+            PassthroughHand.Dominant when HandednessManager.Handedness is Handedness.Right => m_RightHand,
+            PassthroughHand.NonDominant when HandednessManager.Handedness is Handedness.Left => m_RightHand,
+            PassthroughHand.NonDominant when HandednessManager.Handedness is Handedness.Right => m_LeftHand,
+            PassthroughHand.Left => m_LeftHand,
+            PassthroughHand.Right => m_RightHand,
+            _ => throw new System.NotImplementedException()
+        };
+
+        private enum PassthroughHand {
+            Dominant,
+            NonDominant,
+            Left,
+            Right,
+        }
+    }
+}
