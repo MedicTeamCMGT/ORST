a5b618eef1f94dfd8164e7272f52c0551b8b6946
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-11-23 11:06:07 +0100

    Add RayInteractorProvider script
    This script provides an easy way to get a reference to a RayInteractor
    for either the left or right hands, or for dominant and non-dominant
    hands. Without this you would need to keep track of when handedness
    changes and update your references to the new RayInteractor.

---
+++ ORST/Assets/Scripts/Core/Interactions/Handedness/Interactors/RayInteractorProvider.cs
@@ -0,0 +1,30 @@
+﻿using Oculus.Interaction;
+using Oculus.Interaction.Input;
+using ORST.Foundation.Core;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ORST.Core.Interactions {
+    public class RayInteractorProvider : BaseMonoBehaviour, IRayInteractorProvider {
+        [SerializeField] private PassthroughHand m_Hand = PassthroughHand.Dominant;
+        [SerializeField, Required] private RayInteractor m_LeftHand;
+        [SerializeField, Required] private RayInteractor m_RightHand;
+
+        public RayInteractor Value => m_Hand switch {
+            PassthroughHand.Dominant when HandednessManager.Handedness is Handedness.Left => m_LeftHand,
+            PassthroughHand.Dominant when HandednessManager.Handedness is Handedness.Right => m_RightHand,
+            PassthroughHand.NonDominant when HandednessManager.Handedness is Handedness.Left => m_RightHand,
+            PassthroughHand.NonDominant when HandednessManager.Handedness is Handedness.Right => m_LeftHand,
+            PassthroughHand.Left => m_LeftHand,
+            PassthroughHand.Right => m_RightHand,
+            _ => throw new System.NotImplementedException()
+        };
+
+        private enum PassthroughHand {
+            Dominant,
+            NonDominant,
+            Left,
+            Right,
+        }
+    }
+}
