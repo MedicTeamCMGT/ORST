552363261be4fc9378dc8263451e0925d13471d4
Maurice Johannssen <60900273+MauriceJohannssen@users.noreply.github.com>
2022-11-23 10:31:53 +0100

    Implement 'PlaceObject' task

---
+++ ORST/Assets/Scripts/Core/ModuleTasks/GeneralTasks/PlaceObjectTask.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using ORST.Core.ModuleTasks;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ORST.Core.ModuleTasks {
+    public class PlaceObjectTask : ModuleTask {
+        [SerializeField, Required] private GameObject m_ObjectToPlace;
+        [LabelText("[?] Track Only When Running"), Tooltip("If this is enabled then the task will only update while the task is running.")]
+        [SerializeField] private bool m_TrackOnlyWhenRunning = true;
+        private bool m_PlacedObject;
+        private BoxCollider m_BoxCollider;
+
+        protected override void OnModuleTaskStarted() {
+            if (m_TrackOnlyWhenRunning) {
+                m_PlacedObject = false;
+            }
+        }
+
+        protected override ModuleTaskState ExecuteModuleTask() {
+            return m_PlacedObject ? ModuleTaskState.Successful : ModuleTaskState.Running;
+        }
+
+        private void Reset() {
+            m_BoxCollider ??= gameObject.AddComponent<BoxCollider>();
+            m_BoxCollider.isTrigger = true;
+        }
+
+        private void OnTriggerEnter(Collider other) {
+            if ((!m_TrackOnlyWhenRunning || Started) && other.gameObject == m_ObjectToPlace) {
+                m_PlacedObject = true;
+            }
+        }
+    }
+}
