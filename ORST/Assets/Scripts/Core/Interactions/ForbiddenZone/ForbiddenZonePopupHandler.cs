7501dbbb87c80e76261a55ab5a077044a186d363
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-11-27 17:10:14 +0100

    Rework Forbidden Zone mechanics to not use physics system
    This commit reimplements the forbidden zone mechanics to not rely on
    having colliders or rigidbodies on any object besides the trigger
    collider used as the forbidden zone.

---
+++ ORST/Assets/Scripts/Core/Interactions/ForbiddenZone/ForbiddenZonePopupHandler.cs
@@ -0,0 +1,34 @@
+﻿using ORST.Core.UI;
+using ORST.Foundation.Core;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ORST.Core.Interactions {
+    public class ForbiddenZonePopupHandler : BaseMonoBehaviour {
+        [SerializeField, Required] private ForbiddenZone m_ForbiddenZone;
+        [Space]
+        [SerializeField] private string m_Title;
+        [SerializeField] private string m_Message;
+
+        private void OnEnable() {
+            m_ForbiddenZone.EnteredZone += OnEnteredZone;
+            m_ForbiddenZone.ExitedZone += OnExitedZone;
+        }
+
+        private void OnDisable() {
+            m_ForbiddenZone.EnteredZone -= OnEnteredZone;
+            m_ForbiddenZone.ExitedZone -= OnExitedZone;
+        }
+
+        private void OnEnteredZone() {
+            if (!PopupManager.Instance.IsPopupShown()) {
+                PopupManager.Instance.OpenPopup();
+                PopupManager.Instance.DisplayInfo(m_Title, m_Message);
+            }
+        }
+
+        private void OnExitedZone() {
+            PopupManager.Instance.ClosePopup();
+        }
+    }
+}
