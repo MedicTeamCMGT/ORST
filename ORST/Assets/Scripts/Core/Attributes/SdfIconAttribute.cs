68369cbbab04964d3acb60aaa082d6194290fe7a
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-11-27 14:09:54 +0100

    Add SdfIcon attribute

---
+++ ORST/Assets/Scripts/Core/Attributes/SdfIconAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ORST.Core.Attributes {
+    public class SdfIconAttribute : Attribute {
+        public SdfIconType Icon { get; set; }
+
+        public float Padding { get; set; } = 1.0f;
+        public float Margin { get; set; } = -2.0f;
+        public SdfIconAlignment Alignment { get; set; } = SdfIconAlignment.Left;
+
+#if UNITY_EDITOR
+        public string Color { get; set; } = UnityEditor.EditorGUIUtility.isProSkin ? "#FFFFFF" : "#2F2F2F";
+#else
+        public string Color { get; set; }
+#endif
+        public Color GetColor() => ColorUtility.TryParseHtmlString(this.Color, out Color color) ? color : UnityEngine.Color.white;
+
+        public SdfIconAttribute(SdfIconType icon) {
+            this.Icon = icon;
+        }
+    }
+
+    public enum SdfIconAlignment {
+        Left,
+        Right,
+    }
+}
