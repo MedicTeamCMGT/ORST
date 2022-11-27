3a6f1580c29527eb2224c9b6aac0b23f51a1be7f
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-11-27 17:02:58 +0100

    Cleanup TeleportAimLaserVisualBezier

--- ORST/Assets/Scripts/Core/Movement/Teleportation/TeleportAimLaserVisualBezier.cs
+++ ORST/Assets/Scripts/Core/Movement/Teleportation/TeleportAimLaserVisualBezier.cs
@@ -1,90 +1,68 @@
-using System;
-using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine.Serialization;
+using UnityEngine;
 
-public class TeleportAimLaserVisualBezier : TeleportSupport {
-	/// <summary>
-	/// This prefab will be instantiated when the aim visual is awakened, and will be set active when the 
-	/// user is aiming, and deactivated when they are done aiming.
-	/// </summary>
-	[Tooltip("This prefab will be instantiated when the aim visual is awakened, and will be set active when the user is aiming," +
-             " and deactivated when they are done aiming.")]
-	public LineRenderer LaserPrefab;
-    [SerializeField, Range(3, 100)] private int m_Resolution = 500;
+namespace ORST.Core.Movement {
+    public class TeleportAimLaserVisualBezier : TeleportSupport {
+        [SerializeField] private LineRenderer m_LaserPrefab;
+        [SerializeField, Min(3)] private int m_Resolution = 500;
 
-	private readonly Action _enterAimStateAction;
-	private readonly Action _exitAimStateAction;
-	private readonly Action<LocomotionTeleport.AimData> _updateAimDataAction;
-	private LineRenderer _lineRenderer;
-	private Vector3[] _linePoints;
+        private LineRenderer m_LineRenderer;
 
-	public TeleportAimLaserVisualBezier() {
-		_enterAimStateAction = EnterAimState;
-		_exitAimStateAction = ExitAimState;
-		_updateAimDataAction = UpdateAimData;
-	}
-
-	private void EnterAimState() {
-		_lineRenderer.gameObject.SetActive(true);
-	}
-
-	private void ExitAimState() {
-		_lineRenderer.gameObject.SetActive(false);
-	}
-
-	void Awake() {
-		LaserPrefab.gameObject.SetActive(false);
-		_lineRenderer = Instantiate(LaserPrefab);
-	}
-
-	protected override void AddEventHandlers() {
-		base.AddEventHandlers();
-		LocomotionTeleport.EnterStateAim += _enterAimStateAction;
-		LocomotionTeleport.ExitStateAim += _exitAimStateAction;
-		LocomotionTeleport.UpdateAimData += _updateAimDataAction;
-	}
-
-	/// <summary>
-	/// Derived classes that need to use event handlers need to override this method and
-	/// call the base class to ensure all event handlers are removed as intended.
-	/// </summary>
-	protected override void RemoveEventHandlers() {
-		LocomotionTeleport.EnterStateAim -= _enterAimStateAction;
-		LocomotionTeleport.ExitStateAim -= _exitAimStateAction;
-		LocomotionTeleport.UpdateAimData -= _updateAimDataAction;
-		base.RemoveEventHandlers();
-	}
-
-	private void UpdateAimData(LocomotionTeleport.AimData obj) {
-        _lineRenderer.sharedMaterial.color = Color.green;
-
-        //Note: The line renderer is dis-/enabled in the Enter/ExitAimState methods.
-        //   This dis-/enables it based on whether the user is hitting a destination.
-        if (LocomotionTeleport.CurrentIntention != LocomotionTeleport.TeleportIntentions.Aim || !obj.TargetValid) {
-            _lineRenderer.gameObject.SetActive(false);
-            return;
+        private void Awake() {
+            m_LaserPrefab.gameObject.SetActive(false);
+            m_LineRenderer = Instantiate(m_LaserPrefab);
         }
-        _lineRenderer.gameObject.SetActive(true);
 
-		var points = obj.Points;
-        _lineRenderer.positionCount = m_Resolution;
-        var firstElement = points[0];
-        var lastElement = points[^1];
-        LocomotionTeleport.InputHandler.GetAimData(out Ray aimRay);
-        Vector3 inBetween = (lastElement - firstElement) / 2;
+        protected override void AddEventHandlers() {
+            base.AddEventHandlers();
+            LocomotionTeleport.EnterStateAim += EnterAimState;
+            LocomotionTeleport.ExitStateAim += ExitAimState;
+            LocomotionTeleport.UpdateAimData += UpdateAimData;
+        }
 
-        for (int i = 0; i < m_Resolution; i++) {
-            float t = i / (float)(m_Resolution - 1);
-            _lineRenderer.SetPosition(i, GetPoint(firstElement, firstElement + aimRay.direction * inBetween.magnitude, lastElement, t));
-		}
-	}
+        protected override void RemoveEventHandlers() {
+            base.RemoveEventHandlers();
+            LocomotionTeleport.EnterStateAim -= EnterAimState;
+            LocomotionTeleport.ExitStateAim -= ExitAimState;
+            LocomotionTeleport.UpdateAimData -= UpdateAimData;
+        }
 
-    private Vector3 GetPoint(Vector3 P0, Vector3 P1, Vector3 P2, float t) {
-        return Vector3.Lerp(
-            Vector3.Lerp(P0, P1, t),
-            Vector3.Lerp(P1, P2, t),
-            t
-        );
+        private void EnterAimState() {
+            m_LineRenderer.gameObject.SetActive(true);
+        }
+
+        private void ExitAimState() {
+            m_LineRenderer.gameObject.SetActive(false);
+        }
+
+        private void UpdateAimData(LocomotionTeleport.AimData obj) {
+            m_LineRenderer.sharedMaterial.color = Color.green;
+
+            //Note: The line renderer is dis-/enabled in the Enter/ExitAimState methods.
+            //   This dis-/enables it based on whether the user is hitting a destination.
+            if (LocomotionTeleport.CurrentIntention != LocomotionTeleport.TeleportIntentions.Aim || !obj.TargetValid) {
+                m_LineRenderer.gameObject.SetActive(false);
+                return;
+            }
+
+            m_LineRenderer.gameObject.SetActive(true);
+
+            List<Vector3> points = obj.Points;
+            m_LineRenderer.positionCount = m_Resolution;
+            Vector3 firstElement = points[0];
+            Vector3 lastElement = points[^1];
+            LocomotionTeleport.InputHandler.GetAimData(out Ray aimRay);
+
+            for (int i = 0; i < m_Resolution; i++) {
+                float t = i / (float)(m_Resolution - 1);
+                m_LineRenderer.SetPosition(i, GetPoint(firstElement, firstElement + aimRay.direction * ((lastElement - firstElement) / 2).magnitude, lastElement, t));
+            }
+        }
+
+        private static Vector3 GetPoint(Vector3 start, Vector3 control, Vector3 end, float t) {
+            float t0 = (1.0f - t) * (1.0f - t);
+            float t1 = t * t;
+            return control + t0 * (start - control) + t1 * (end - control);
+        }
     }
 }
