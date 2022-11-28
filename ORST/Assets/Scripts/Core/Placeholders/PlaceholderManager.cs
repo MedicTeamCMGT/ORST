fd6e5d68c4ab7a1ae5db69099e2cc938f9cd37ee
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-11-28 11:00:56 +0100

    Add PlaceholderManager
    This class allows to register placeholders which can be used to replace
    values in strings.
    
    The placeholders are stored in a dictionary and can be accessed by the
    key of the placeholder.
    
    The placeholder manager also provides a method to resolve a string
    by replacing all placeholders with their values. This is useful for
    displaying dynamic text in the UI.

---
+++ ORST/Assets/Scripts/Core/Placeholders/PlaceholderManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace ORST.Core.Placeholders {
+    public static class PlaceholderManager {
+        private static readonly Dictionary<string, string> s_Placeholders = new();
+
+        /// <summary>
+        /// Tries to get the value of the given placeholder key.
+        /// </summary>
+        /// <param name="key">The key of the placeholder.</param>
+        /// <param name="value">Out parameter for the value of the placeholder.</param>
+        /// <returns><see langword="true"/> if the placeholder exists, otherwise <see langword="false"/>.</returns>
+        public static bool TryGetPlaceholder(string key, out string value) {
+            return s_Placeholders.TryGetValue(key, out value);
+        }
+
+        /// <summary>
+        /// Registers a new placeholder with the given key and value.
+        /// </summary>
+        /// <param name="key">The key of the placeholder.</param>
+        /// <param name="value">The value of the placeholder.</param>
+        public static void AddPlaceholder(string key, string value) {
+            s_Placeholders[key] = value;
+        }
+
+        /// <summary>
+        /// Removes the placeholder with the given key.
+        /// </summary>
+        /// <param name="key">The key of the placeholder.</param>
+        /// <returns><see langword="true"/> if the placeholder was removed, otherwise <see langword="false"/>.</returns>
+        public static bool RemovePlaceholder(string key) {
+            return s_Placeholders.Remove(key);
+        }
+
+        /// <summary>
+        /// Resolves the given input string by replacing all placeholders with their values.
+        /// </summary>
+        /// <param name="input">The input string to resolve.</param>
+        /// <returns>The resolved string.</returns>
+        public static string Resolve(string input) {
+            foreach ((string key, string value) in s_Placeholders) {
+                input = input.Replace(key, value);
+            }
+
+            return input;
+        }
+    }
+}
