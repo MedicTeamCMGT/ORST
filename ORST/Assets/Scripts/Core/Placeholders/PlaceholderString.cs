db008dd699f63cd1f1869bb9674509a442137a69
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-12-01 10:23:41 +0100

    Add PlaceholderString script

---
+++ ORST/Assets/Scripts/Core/Placeholders/PlaceholderString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ORST.Foundation;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ORST.Core.Placeholders {
+    public class PlaceholderString : BaseMonoBehaviour {
+        [SerializeField, Required] private string m_PlaceholderKey;
+        [SerializeField] private UnityEvent<string> m_SetText;
+
+        private void OnEnable() {
+            UpdateText();
+        }
+
+        private void Start() {
+            UpdateText();
+        }
+
+        private void UpdateText() {
+            if (PlaceholderManager.TryGetPlaceholder(m_PlaceholderKey, out string text)) {
+                m_SetText.Invoke(text);
+            }
+        }
+    }
+}
