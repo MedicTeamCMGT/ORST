5c882fa903a6ed565e35d44e0521e99696ab6566
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-12-01 12:56:01 +0100

    Add support for default value in PlaceholderString

--- ORST/Assets/Scripts/Core/Placeholders/PlaceholderString.cs
+++ ORST/Assets/Scripts/Core/Placeholders/PlaceholderString.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using ORST.Foundation;
+﻿using ORST.Foundation;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,6 +6,10 @@ using UnityEngine.Events;
 namespace ORST.Core.Placeholders {
     public class PlaceholderString : BaseMonoBehaviour {
         [SerializeField, Required] private string m_PlaceholderKey;
+        [SerializeField] private bool m_UseDefaultValue;
+        [SerializeField, ShowIf(nameof(m_UseDefaultValue))]
+        private string m_DefaultValue;
+        [Space]
         [SerializeField] private UnityEvent<string> m_SetText;
 
         private void OnEnable() {
@@ -18,6 +21,11 @@ namespace ORST.Core.Placeholders {
         }
 
         private void UpdateText() {
+            if (m_UseDefaultValue) {
+                m_SetText.Invoke(PlaceholderManager.GetValueOrDefault(m_PlaceholderKey, m_DefaultValue));
+                return;
+            }
+
             if (PlaceholderManager.TryGetValue(m_PlaceholderKey, out string text)) {
                 m_SetText.Invoke(text);
             }
