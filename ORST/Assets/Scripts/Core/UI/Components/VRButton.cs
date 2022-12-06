06d8dec30374e87f5d4f0aaa344b036e02a21907
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-12-06 10:36:21 +0100

    Add VRButton wrapper script

---
+++ ORST/Assets/Scripts/Core/UI/Components/VRButton.cs
@@ -0,0 +1,19 @@
+﻿using ORST.Foundation;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ORST.Core.UI.Components {
+    [RequireComponent(typeof(Button), typeof(CanvasGroup))]
+    public class VRButton : BaseMonoBehaviour {
+        private Button m_Button;
+        private CanvasGroup m_CanvasGroup;
+
+        public Button Button => m_Button;
+        public CanvasGroup CanvasGroup => m_CanvasGroup;
+
+        private void Awake() {
+            m_Button = GetComponent<Button>();
+            m_CanvasGroup = GetComponent<CanvasGroup>();
+        }
+    }
+}
