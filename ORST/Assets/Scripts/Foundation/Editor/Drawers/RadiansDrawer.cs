16a20f8a59a3449b77c7c83e520ee8449af9d8b2
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-12-07 14:56:11 +0100

    Add strong types for Degrees and Radians

---
+++ ORST/Assets/Scripts/Foundation/Editor/Drawers/RadiansDrawer.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using ORST.Foundation.StrongTypes;
+using Sirenix.OdinInspector.Editor;
+using UnityEngine;
+
+namespace ORST.Foundation.Editor.Drawers {
+    public class RadiansDrawer : OdinValueDrawer<Radians> {
+        protected override void DrawPropertyLayout(GUIContent label) {
+            Property.Children.First().Draw(label);
+        }
+    }
+}
