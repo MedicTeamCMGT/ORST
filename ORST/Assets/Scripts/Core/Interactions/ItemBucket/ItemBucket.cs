5cce72d78a8ea2b9fbc32f3138744ba76d6fbf17
Maurice Johannssen <60900273+MauriceJohannssen@users.noreply.github.com>
2022-12-14 11:57:06 +0100

    Implement item bucket (Draft)

---
+++ ORST/Assets/Scripts/Core/Interactions/ItemBucket/ItemBucket.cs
@@ -0,0 +1,47 @@
+﻿using Oculus.Interaction;
+using Oculus.Interaction.HandGrab;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ORST.Core {
+    [RequireComponent(typeof(BoxCollider))]
+    public class ItemBucket : MonoBehaviour {
+        public Action ItemBucketFilled;
+
+        [SerializeField] private List<GameObject> m_Items;
+
+        private void OnTriggerEnter(Collider collider) {
+            if (!collider.CompareTag("Interactable") || !m_Items.Contains(collider.gameObject)) {
+                return;
+            }
+
+            HandGrabInteractable interactable = collider.GetComponentInChildren<HandGrabInteractable>();
+
+            if (interactable == null) {
+                return;
+            }
+
+            //If it's currently still in the user's hand, hook the remove function up to the pointer event and check for unselect.
+            //Otherwise; remove item immediately.
+            if (interactable.State == InteractableState.Select) {
+                interactable.WhenPointerEventRaised += pointerEvent => {
+                    if (pointerEvent.Type == PointerEventType.Unselect) {
+                        RemoveItem(collider.gameObject);
+                    }
+                };
+
+                return;
+            }
+
+            RemoveItem(collider.gameObject);
+        }
+
+        private void RemoveItem(GameObject gameObject) {
+            m_Items.Remove(gameObject);
+            if (m_Items.Count == 0) {
+                ItemBucketFilled?.Invoke();
+            }
+        }
+    }
+}
