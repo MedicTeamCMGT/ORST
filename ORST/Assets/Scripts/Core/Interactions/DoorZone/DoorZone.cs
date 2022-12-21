1fca9414ae9a56efc315df01c04feb52fbd325f6
Maurice Johannssen <60900273+MauriceJohannssen@users.noreply.github.com>
2022-12-21 14:11:12 +0100

    Implement sensor to door zone

--- ORST/Assets/Scripts/Core/Interactions/DoorZone/DoorZone.cs
+++ ORST/Assets/Scripts/Core/Interactions/DoorZone/DoorZone.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using Oculus.Interaction;
@@ -7,23 +7,47 @@ using ORST.Core.ModuleTasks;
 using ORST.Core.UI;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
 
 namespace ORST.Core.Interactions {
     public class DoorZone : MonoBehaviour {
         public event Action ExitedDoor;
-        [SerializeField, Required] private HandGrabInteractable m_DoorHandle;
         [SerializeField, Required] int m_SceneIndex;
+
+        [SerializeField] private DoorInteractor m_DoorInteractor = DoorInteractor.Doorhandle;
+
+        [Header("Door Handle")]
+        [SerializeField, Required] private HandGrabInteractable m_DoorHandle;
+
+        [Header("Hand Sensor")]
+        [SerializeField, Required] private Transform m_HandSensor;
+        [SerializeField, Range(0.2f, 10.0f)] private float m_SensorDetectionDistance;
+
         private OneGrabRotateTransformer m_DoorHandleRotateTransformer;
         private bool m_DoorsUnlocked;
         private bool m_TransitionStarted;
         private GameObject m_Player;
+        private Transform m_DominantHand;
+        private Transform m_NonDominantHand;
+        private bool m_PlayerInZoneNoTasks;
 
         private void Start() {
-            if (m_DoorHandle != null) {
+            if (m_DoorInteractor == DoorInteractor.Doorhandle) {
+                Assert.IsNotNull(m_DoorHandle, "Hand sensor is null");
                 m_DoorHandle.WhenPointerEventRaised += ProcessPointerEvent;
+                //Skipping this assignment is valid since it is only used in the event handler method.
                 m_DoorHandleRotateTransformer = m_DoorHandle.transform.GetComponent<OneGrabRotateTransformer>();
+            } else if (m_DoorInteractor == DoorInteractor.Sensor) {
+                Assert.IsNotNull(m_HandSensor, "Hand sensor is null");
+                //Cache hand references
+                m_DominantHand = HandednessManager.DominantHand.transform;
+                m_NonDominantHand = HandednessManager.NonDominantHand.transform;
             }
+        }
+
+        private void Update() {
+            CheckSensor(); //Do bool check here -> less overhead
         }
 
         private void OnDestroy() {
@@ -43,17 +67,36 @@ namespace ORST.Core.Interactions {
             if (!m_DoorsUnlocked) {
                 PopupManager.Instance.DisplayTasks(remainingTasks);
                 PopupManager.Instance.OpenPopup();
+                return;
             }
+
+            m_PlayerInZoneNoTasks = true;
         }
 
         private void OnTriggerExit(Collider other) {
             if (m_Player != null && m_Player == other.gameObject) {
                 PopupManager.Instance.ClosePopup();
                 m_Player = null;
+                m_PlayerInZoneNoTasks = false;
             }
         }
 
-        private void DoorHandleMoved() {
+        private void CheckSensor() {
+            if (m_DoorInteractor != DoorInteractor.Sensor) {
+                return;
+            }
+
+            if (m_PlayerInZoneNoTasks) {
+                if ((m_HandSensor.position - m_DominantHand.position).magnitude < m_SensorDetectionDistance ||
+                    (m_HandSensor.position - m_NonDominantHand.position).magnitude < m_SensorDetectionDistance) {
+                    m_PlayerInZoneNoTasks = false;
+                    ExitedDoor?.Invoke();
+                }
+
+            }
+        }
+
+        private void InitiateSceneTransition() {
             if (!m_DoorsUnlocked) {
                 return;
             }
@@ -80,7 +123,12 @@ namespace ORST.Core.Interactions {
                 return;
             }
 
-            DoorHandleMoved();
+            InitiateSceneTransition();
+        }
+
+        enum DoorInteractor {
+            Doorhandle,
+            Sensor
         }
     }
 }
