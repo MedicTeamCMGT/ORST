281020deb2180fd7728bf9c9672a7baebe121786
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2023-01-12 15:39:40 +0100

    Implement Hallway scene functionality

--- ORST/Assets/Scripts/Core/LearningModules/NameTag/NameTag.cs
+++ ORST/Assets/Scripts/Core/LearningModules/NameTag/NameTag.cs
@@ -1,5 +1,15 @@
-﻿namespace ORST.Core.LearningModules {
+﻿using ORST.Core.Placeholders;
+
+namespace ORST.Core.LearningModules {
     public static class NameTag {
-        public static NameTagKind Kind { get; internal set; }
+        private static NameTagKind s_NameTagKind;
+        public static NameTagKind Kind {
+            get => s_NameTagKind;
+            internal set {
+                s_NameTagKind = value;
+                PlaceholderManager.AddPlaceholder("{CORRECT COMPANY}", s_NameTagKind == NameTagKind.DePuy ? "J&J DePuy Synthes" : "J&J Ethicon");
+                PlaceholderManager.AddPlaceholder("{WRONG COMPANY}", s_NameTagKind == NameTagKind.DePuy ? "J&J Ethicon" : "J&J DePuy Synthes");
+            }
+        }
     }
 }
