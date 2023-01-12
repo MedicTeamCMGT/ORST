98af321b0d301e0e74cdcee413ad796dbf71cc99
0MADXX <marcosrane@gmail.com>
2023-01-12 13:26:33 +0100

    Add Custom sounds Integrations TP

--- ORST/Assets/Scripts/Core/Movement/Teleportation/TeleportAudioHandler.cs
+++ ORST/Assets/Scripts/Core/Movement/Teleportation/TeleportAudioHandler.cs
@@ -1,61 +1,42 @@
 ﻿using System;
 using UnityEngine;
 
-namespace ORST.Core.Movement
-{
-
-    public class TeleportAudioHandler : TeleportSupport
-    {
+namespace ORST.Core.Movement {
+    public class TeleportAudioHandler : TeleportSupport {
         [SerializeField] private AudioClipInfo m_EnterAim;
         [SerializeField] private AudioClipInfo m_CancelAim;
         [SerializeField] private AudioClipInfo m_Teleporting;
-
+        [SerializeField] private AudioClipInfo m_IntersectEnter;
+        [SerializeField] private AudioClipInfo m_IntersectExit;
         private AudioClipInfo m_CurrentAudioClip;
         private AudioSource m_PlayerAudioSource;
         private AudioSource m_DestAudioSource;
 
-        private LocomotionTeleport.AimData m_Obj;
-
-
-        private AdvancedLocomotionTeleport m_AdvancedLocomotionTeleport;
-
-        //private LocomotionTeleport m_currentState;
-
-        protected override void OnEnable()
-        {
+        protected override void OnEnable() {
             base.OnEnable();
-            if (LocomotionTeleport is AdvancedLocomotionTeleport teleport)
-            {
+            if (LocomotionTeleport is AdvancedLocomotionTeleport teleport) {
                 teleport.AudioHandler = this;
-                m_AdvancedLocomotionTeleport = teleport;
-                //teleport.EnteredIntersection += IntersectEnterState;
-                //teleport.ExitedIntersection += IntersectExitState;
+                teleport.EnteredIntersection += IntersectEnterState;
+                teleport.ExitedIntersection += IntersectExitState;
             }
 
             m_PlayerAudioSource = gameObject.AddComponent<AudioSource>();
             m_PlayerAudioSource.playOnAwake = false;
-
-            //m_currentState = LocomotionTeleport.States.Ready;
-
-
         }
 
-        protected override void OnDisable()
-        {
+        protected override void OnDisable() {
             base.OnDisable();
             Destroy(m_PlayerAudioSource);
         }
 
-        protected override void AddEventHandlers()
-        {
+        protected override void AddEventHandlers() {
             base.AddEventHandlers();
             LocomotionTeleport.EnterStateAim += EnterAimState;
             LocomotionTeleport.ExitStateAim += ExitAimState;
             LocomotionTeleport.EnterStateTeleporting += TeleportState;
         }
 
-        protected override void RemoveEventHandlers()
-        {
+        protected override void RemoveEventHandlers() {
             base.RemoveEventHandlers();
             LocomotionTeleport.EnterStateAim -= EnterAimState;
             LocomotionTeleport.ExitStateAim -= ExitAimState;
@@ -65,38 +46,28 @@ namespace ORST.Core.Movement
         private void EnterAimState() => OnStateSet(m_EnterAim);
         private void ExitAimState() => OnStateSet(m_CancelAim);
         private void TeleportState() => OnStateSet(m_Teleporting);
+        private void IntersectEnterState() => OnStateSet(m_IntersectEnter);
+        private void IntersectExitState() => OnStateSet(m_IntersectExit);
 
-        private void OnStateSet(AudioClipInfo audioClip)
-        {
-            if (m_PlayerAudioSource.isPlaying && audioClip.Priority < m_CurrentAudioClip.Priority)
-            {
+
+        private void OnStateSet(AudioClipInfo audioClip) {
+            if (m_PlayerAudioSource.isPlaying && audioClip.Priority < m_CurrentAudioClip.Priority) {
                 return;
             }
 
-            Debug.LogError(AdvancedLocomotionTeleport.Instance.CurrentIntention);
-            Debug.LogError(LocomotionTeleport.TeleportIntentions.Aim);
-            Debug.LogError(m_Obj.TargetValid);
-
-
-            if (AdvancedLocomotionTeleport.Instance.CurrentIntention == LocomotionTeleport.TeleportIntentions.Aim || m_Obj.TargetValid) {
-
-                m_CurrentAudioClip = m_EnterAim;
-                m_PlayerAudioSource.clip = m_CurrentAudioClip.AudioClip;
-                m_PlayerAudioSource.loop = m_CurrentAudioClip.Loop;
-                m_PlayerAudioSource.Play();
-            } else {
-                m_PlayerAudioSource.Stop();
-            }
+            m_CurrentAudioClip = audioClip;
+            m_PlayerAudioSource.clip = m_CurrentAudioClip.AudioClip;
+            m_PlayerAudioSource.loop = m_CurrentAudioClip.Loop;
+            m_PlayerAudioSource.Play();
         }
+        
 
-        public void SetDestAudioSource(AudioSource audioSource)
-        {
+        public void SetDestAudioSource(AudioSource audioSource) {
             m_DestAudioSource = audioSource;
         }
 
         [Serializable]
-        private struct AudioClipInfo
-        {
+        private struct AudioClipInfo {
             public AudioClip AudioClip;
             public bool Loop;
             public ushort Priority;
