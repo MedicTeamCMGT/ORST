0f1b7272b42d3ccba1c9f8505dd53855b3b2a060
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2023-01-17 12:13:30 +0100

    Add task board

---
+++ ORST/Assets/Scripts/Core/UI/TaskBoard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using ORST.Core.ModuleTasks;
+using ORST.Foundation;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+
+namespace ORST.Core.UI {
+    public class TaskBoard : BaseMonoBehaviour {
+        [SerializeField, Required] private List<TextMeshProUGUI> m_TaskLabels;
+
+        private void Awake() {
+            foreach (TextMeshProUGUI label in m_TaskLabels) {
+                label.gameObject.SetActive(false);
+                label.text = string.Empty;
+            }
+        }
+
+        private void Start() {
+            UpdateTaskBoard();
+        }
+
+        private void OnEnable() {
+            ModuleTasksManager.Instance.TaskCompleted += OnTaskCompleted;
+        }
+
+        private void OnDisable() {
+            ModuleTasksManager.Instance.TaskCompleted -= OnTaskCompleted;
+        }
+
+        private void OnTaskCompleted(ModuleTask task) => UpdateTaskBoard();
+
+        private void UpdateTaskBoard() {
+            List<ModuleTask> remainingTasks = ModuleTasksManager.Instance.GetRemainingTasks();
+            int displayedCount = Mathf.Min(remainingTasks.Count, m_TaskLabels.Count);
+
+            for (int i = 0; i < displayedCount; i++) {
+                m_TaskLabels[i].text = remainingTasks[i].gameObject.name;
+                m_TaskLabels[i].gameObject.SetActive(true);
+            }
+
+            for (int i = displayedCount; i < m_TaskLabels.Count; i++) {
+                m_TaskLabels[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
