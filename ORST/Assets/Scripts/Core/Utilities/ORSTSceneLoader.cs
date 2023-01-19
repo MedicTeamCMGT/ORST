18ef5a7bd1ac22a04ab17f88772710444e7224fd
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2023-01-19 15:31:58 +0100

    Implement main menu

---
+++ ORST/Assets/Scripts/Core/Utilities/ORSTSceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ORST.Core.Utilities {
+    public class ORSTSceneLoader : MonoBehaviour {
+        private bool m_IsLoadingScene;
+
+        public void LoadSceneFade(int sceneIndex) {
+            if (m_IsLoadingScene) {
+                return;
+            }
+
+            m_IsLoadingScene = true;
+            StartCoroutine(LoadSceneCoroutine(sceneIndex, true));
+        }
+
+        public void LoadScene(int sceneIndex) {
+            if (m_IsLoadingScene) {
+                return;
+            }
+
+            m_IsLoadingScene = true;
+            StartCoroutine(LoadSceneCoroutine(sceneIndex, false));
+        }
+
+        private IEnumerator LoadSceneCoroutine(int sceneIndex, bool fadeToBlack) {
+            if (fadeToBlack) {
+                OVRScreenFade.instance.FadeOut();
+                yield return new WaitUntil(() => Mathf.Abs(OVRScreenFade.instance.currentAlpha - 1.0f) < 0.01f);
+            }
+
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
